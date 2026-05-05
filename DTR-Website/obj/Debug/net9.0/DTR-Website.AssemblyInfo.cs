9a5dea61c9f0122f76b44afb3b43bd99c70b1200
Tai <148828091+TyriseManguiat@users.noreply.github.com>
2026-05-05 14:38:13 +0800

    Update build artifacts and generate package-lock.json for project dependencies

--- DTR-Website/obj/Debug/net9.0/DTR-Website.AssemblyInfo.cs
+++ DTR-Website/obj/Debug/net9.0/DTR-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTR-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a307457462078be78d73b14ccb7cb1ca01e26eea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a832e375c65358c7df15636164aa3b230628a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTR-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTR-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
